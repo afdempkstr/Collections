9973a990b42a173ae02bc77b7a80b9ef7f50ac64
Kostas Stroggylos <kostas.stroggylos@peoplecert.onmicrosoft.com>
2018-11-06 19:46:59 +0200

    removed list-related code before talking about other collection types

--- Collections/Program.cs
+++ Collections/Program.cs
@@ -7,47 +7,6 @@ namespace Collections
     {
         static void Main(string[] args)
         {
-            //A(5.0);
-            //A(5.0f);
-            A(5);
-            //var list = new List<int>();
-            //list.Add(5);
-            //list.Add(7);
-            //for (int i = 0; i < 5; i++)
-            //{
-            //    list.Add(i);
-            //}
-
-            //list[0] = 3;
-
-            //Show(list);
-
-            //list.Remove(3);
-
-            //var floatList = new List<float>();
-            //for (int j = 0; j < 5; j++)
-            //{
-            //    floatList.Add(j);
-            //}
-
-            //Show(list);
-            //Show(floatList);
-
-            //// 3 7 0 1 2 3 4
-
-            //list.RemoveAll(Check);
-
-            //list.RemoveAll(i => Check(i));
-
-            //list.RemoveAll(i => i % 2 == 0);
-
-            //list.RemoveAll(i =>
-            //{
-            //    return i % 2 == 0;
-            //});
-
-
-
 
 
             Console.Read();
@@ -57,19 +16,6 @@ namespace Collections
 
 
 
-
-
-
-        }
-
-        static bool Check(int i)
-        {
-            return i % 2 == 0;
-        }
-
-        static bool Check<T>(T item)
-        {
-            return item != null;
         }
 
         static void Show<T>(List<T> theList)
@@ -80,13 +26,5 @@ namespace Collections
             }
         }
 
-
-        static void A(double a)
-        { }
-
-        static void A(float a)
-        { }
-
-        
     }
 }
