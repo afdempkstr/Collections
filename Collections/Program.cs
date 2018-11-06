bc3e4c52f0594d85b1fd21a0e8a570fe602d1fb7
Kostas Stroggylos <kostas.stroggylos@peoplecert.onmicrosoft.com>
2018-11-06 20:31:32 +0200

    By Ref example

--- Collections/Program.cs
+++ Collections/Program.cs
@@ -7,6 +7,37 @@ namespace Collections
     {
         static void Main(string[] args)
         {
+            var dict = new Dictionary<string, int>();
+
+            dict.Add("kostas", 2105555555);
+            dict.Add("george", 2109999999);
+
+            dict.ContainsKey("test");
+
+            dict["nick"] = 697123123;
+
+            dict["nick"] = 333;
+
+            foreach (var item in dict)
+            {
+                Console.Write($"{item.Key} => {item.Value} ");
+            }
+
+            var s = dict["george"];
+
+            int testValue;
+            bool exists = dict.TryGetValue("test", out testValue);
+            testValue = 3;
+
+
+            var badIdea = new Dictionary<object, object>();
+            var goodOldDays = new System.Collections.Hashtable();
+
+            int a = 5;
+            Increment(a);
+            Console.WriteLine(a);
+            IncByRef(ref a);
+            Console.WriteLine(a);
 
 
             Console.Read();
@@ -24,6 +55,16 @@ namespace Collections
             {
                 Console.Write(item + " ");
             }
+        }
+
+        static void Increment(int x)
+        {
+            x = x + 1;
+        }
+
+        static void IncByRef(ref int x)
+        {
+            x = x + 1;
         }
 
     }
